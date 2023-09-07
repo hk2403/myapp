9cfd94def8068b9a6e4696cc31f072f241f88028
Hemanth Kumar <itsmehemanthk@gmail.com.com>
2023-09-07 11:00:42 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public loginForm:FormGroup=new FormGroup({
+    email:new FormControl(),
+    password: new FormControl()
+  });
+
+  constructor(private _loginService:LoginService){}
+  login(){
+    this._loginService.doLogin(this.loginForm.value).subscribe(
+      (data:any)=>{
+        alert("Login Success");
+        
+      },
+      (err:any)=>{
+        alert("Login Failed");
+      }
+    )
+
+  }
 
 }
